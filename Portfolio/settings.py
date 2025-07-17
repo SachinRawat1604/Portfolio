1b36b78d2dcab9f9bf9e5c24b2159a7dbe931a43
SachinRawat1604 <sachinrawat4666@gmail.com>
2025-07-17 12:33:31 +0530

    Little update

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["your‑service.onrender.com", ".onrender.com"]
+ALLOWED_HOSTS = ["sachin-portfolio.onrender.com", ".onrender.com"]
 
 
 # Application definition
@@ -137,6 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
